322bd630b2972a5f5c15451744295de7e18767fa
neel.roshania@zoominfo.com <neel.roshania@zoominfo.com>
2023-02-13 17:55:50 -0500

    Included argument parsing

--- scripts/run_script.py
+++ scripts/run_script.py
@@ -10,7 +10,12 @@ if __name__ == "__main__":
     LOGGER.info('testing scripted implementation')
 
     # parse arguments
-    specific_func('Module setup! (You shouldn\'t see this log on the console)')
+    parser = argparse.ArgumentParser()
+    parser.add_argument("config", type=str, action="store", default="conf/config.yaml", nargs="?")
+    parser.add_argument("--optional", "-o", action="store", type=str, default=8000)
+    args = parser.parse_args()
+
+    specific_func(f'Module setup! (You shouldn\'t see this log on the console) - args:{args}')
     
 
     
