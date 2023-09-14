c6a989707265c104a1bedeaac52d0b0a9b9bfe6f
Neel Roshania <nroshania@gmail.com>
2023-09-13 22:01:38 -0400

    Update funcs.py

--- src/python_template/funcs.py
+++ src/python_template/funcs.py
@@ -4,6 +4,15 @@ import logging
 
 LOGGER = logging.getLogger(__name__) # this will call the logger python_template.funcs
 
+# basic generator definition
+def gen(l):
+    """
+     - equivalent to (_l for _l in l)
+     - https://realpython.com/introduction-to-python-generators/
+    """
+    for _l in l:
+        yield _l
+
 def specific_func(text:str) -> None:
 
     """
